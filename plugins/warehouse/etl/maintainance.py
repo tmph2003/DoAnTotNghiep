87c77d0bb4801b2c10e5c2d8ebd06ce03c2919ba
tmph2003 <tmph2003@gmail.com>
2024-11-29 01:27:57 +0700

    refactor code

--- plugins/warehouse/etl/maintainance.py
+++ plugins/warehouse/etl/maintainance.py
@@ -1,7 +1,6 @@
 from logging import Logger
-
+from plugins.config import config
 from plugins.warehouse.common.trino_helper import TrinoHelper
-
 
 class Maintenance:
     def __init__(self, logger: Logger, trino: TrinoHelper, schema: str):
@@ -18,3 +17,33 @@ class Maintenance:
             self.trino.execute(f"ALTER TABLE iceberg.{self.schema}.{table[0]} EXECUTE expire_snapshots(retention_threshold => '3d')")
             # The orphan files (not referenced by any metadata ) will be removed after 3 days.
             self.trino.execute(f"ALTER TABLE iceberg.{self.schema}.{table[0]} EXECUTE remove_orphan_files(retention_threshold => '3d')")
+
+        rows = self.trino.execute(f"""
+            SELECT a.hl_agent_info
+            FROM {config.HIVE_METASTORE_CATALOG}.{config.HIVE_METASTORE_SCHEMA}.hive_locks a
+            WHERE a.hl_acquired_at IS NOT NULL
+        """)[0]
+        # Lặp qua các bản ghi để kiểm tra query state
+        for hl_agent_info in rows:
+            state = self.trino.execute(f"""
+                SELECT 
+                    CASE 
+                        WHEN EXISTS (
+                            SELECT *
+                            FROM system.runtime.queries a
+                            WHERE a.query_id = '{hl_agent_info}'
+                        ) THEN (
+                            SELECT state
+                            FROM system.runtime.queries a
+                            WHERE a.query_id = '{hl_agent_info}'
+                        )
+                        ELSE 'NOT_EXISTS'
+                    END
+            """)[0][0]
+
+            # Nếu trạng thái là "FINISHED" hoặc query không tồn tại
+            if state == "FINISHED" or state == "NOT_EXISTS":
+                self.trino.execute(f"""
+                    DELETE FROM {config.HIVE_METASTORE_CATALOG}.{config.HIVE_METASTORE_SCHEMA}.hive_locks
+                    WHERE hl_agent_info = '{hl_agent_info}'
+                """)
